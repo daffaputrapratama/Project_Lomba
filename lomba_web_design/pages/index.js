dd4461dc673816575e4c8f26b3cbf64c57c3ae54
Daffa <daffaerpan@gmail.com>
2022-06-04 12:10:53 +0700

    adding fonts

--- lomba_web_design/pages/index.js
+++ lomba_web_design/pages/index.js
@@ -12,10 +12,10 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
+        <h1 className='text-[70px] font-Inter font-medium'>Lorem Ipsum Dolor si <span className='text-blue-500 font-bold'>Jamet🌝</span> </h1>
+   
+        
+      
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
@@ -23,29 +23,29 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h2 className='font-Montserrat font-semibold mb-2'>Ini Monserrat &rarr;</h2>
+            <p className='font-Montserrat mb-2 font-medium'>Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h2 className='font-Mono font-semibold mb-2'>Ini Mono &rarr;</h2>
+            <p className='font-Mono  mb-2'>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h2 className='font-Poppins font-semibold '>Ini Poppins++++ &rarr;</h2>
+            <p className='font-Poppins font-medium'>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
-            <p>
+            <h2 className='font-Inter font-semibold' >Ini Inter &rarr;</h2>
+            <p className='font-Inter font-medium'>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
--- lomba_web_design/tailwind.config.js
+++ lomba_web_design/tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+        'Mono' : 'Roboto Mono',
+        'Poppins' : 'Poppins',
+        'Montserrat' : 'Montserrat',
+        'Inter' : 'Inter'
+
+
+      },
+    },
   },
   plugins: [],
 }
