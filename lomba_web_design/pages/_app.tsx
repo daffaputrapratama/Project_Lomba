48da1770269ce146b8f96ba3576a23ecc22c4e5c
ghalib77 <muhamadghalib7@gmail.com>
2022-06-05 10:11:34 +0700

    refactor: change interface of _app.tsx function

--- lomba_web_design/pages/_app.tsx
+++ lomba_web_design/pages/_app.tsx
@@ -1,7 +1,8 @@
 import "../styles/globals.css";
 import { useRouter } from "next/router";
+import { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: any) {
+function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   return <Component key={router.asPath} {...pageProps} />;
 }
