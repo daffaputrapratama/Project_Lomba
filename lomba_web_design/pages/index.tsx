eb270f51ecadb894e9ae465c403ba9733d88015d
ghalib77 <muhamadghalib7@gmail.com>
2022-06-05 10:11:52 +0700

    test : speachSyntesis test

--- lomba_web_design/pages/index.tsx
+++ lomba_web_design/pages/index.tsx
@@ -1,5 +1,13 @@
-function Home() {
-  return <div>hello</div>;
-}
+import { NextPage } from "next";
+
+const handleOnClick = (e: any) => {
+  const { target } = e;
+  const utterance = new SpeechSynthesisUtterance(`${target.innerText}`);
+  speechSynthesis.speak(utterance);
+};
+
+const Home: NextPage = () => {
+  return <div onClick={handleOnClick.bind(this)}>hello</div>;
+};
 
 export default Home;
